7be4fc88dc29283bf9c37f428c5390d78084fd85
7w7Lufy <yanhmau@outlook.com>
2026-02-24 11:35:28 -0600

    Agregar carpeta Desarrollo_Software

---
+++ microservice.sql
@@ -0,0 +1,18 @@
+-- Database: microservice
+
+-- DROP DATABASE IF EXISTS microservice;
+
+CREATE DATABASE microservice
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Mexico.1252'
+    LC_CTYPE = 'Spanish_Mexico.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+create table prueba
+	nombre varchar(25),
+	numero int;
